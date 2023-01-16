0baff58ebd8a114deb8203ce307e17c890d01fd0
mojihj <mojtaba.honarjoo@gmail.com>
2023-01-16 19:44:44 +0330

    Seventh

---
+++ aval.py
@@ -0,0 +1,22 @@
+# Taeen adad aval
+from itertools import count
+from typing import Counter
+
+
+n= int (input("Please Enter Your Number:"))
+def is_prime (n):
+    aval=True
+    for i in range(2,n):
+        if n % i ==0:
+            aval=False
+    return aval
+if is_prime (n)==True:
+    print ("The number is prime")
+else:
+     print ("The number is NOT prime")
+
+
+
+    
+
+
